2ea36cb389d95f64e6cc428671816751afec17df
capolimia <capolimia@gmail.com>
2025-11-29 17:37:00 -0500

    Cloudrun deploy script

--- deploy-cloudrun.bat
+++ deploy-cloudrun.bat
@@ -26,4 +26,4 @@ gcloud run deploy meal-prep-backend ^
 echo Deployment complete!
 echo Update your frontend environment.prod.ts with the Cloud Run URL
 
-@REM gcloud run deploy meal-prep-backend --image gcr.io/capstone-478122/meal-prep-backend --platform managed --region us-central1 --allow-unauthenticated --port 8080 --memory 2Gi --timeout 600 --set-env-vars GOOGLE_GENAI_USE_VERTEXAI=True,GOOGLE_CLOUD_PROJECT=capstone-478122,GOOGLE_CLOUD_LOCATION=us-central1,GOOGLE_CLOUD_AGENT_ENGINE_ID=4566241003828150272,GOOGLE_CLOUD_AGENT_ENGINE_ENABLE_TELEMETRY=True,TIME_ZONE=America/New_York
+@REM gcloud run deploy meal-prep-backend --image gcr.io/capstone-478122/meal-prep-backend --platform managed --region us-central1 --allow-unauthenticated --port 8080 --memory 2Gi --timeout 600 --set-env-vars "GOOGLE_GENAI_USE_VERTEXAI=True,GOOGLE_CLOUD_PROJECT=capstone-478122,GOOGLE_CLOUD_LOCATION=us-central1,GOOGLE_CLOUD_AGENT_ENGINE_ID=4566241003828150272,GOOGLE_CLOUD_AGENT_ENGINE_ENABLE_TELEMETRY=True,TIME_ZONE=America/New_York"
